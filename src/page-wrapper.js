5906a095ae9c8af15fbc0d2bfc151ad8face1dd7
Christopher Biscardi <chris@christopherbiscardi.com>
2020-10-03 23:07:17 -0700

    a site

---
+++ src/page-wrapper.js
@@ -0,0 +1,5 @@
+import { h } from "preact";
+
+export default (props) => {
+  return <div id="wrapper">{props.children}</div>;
+};
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { h, Fragment } from "preact";
+import { Helmet } from "react-helmet";
+
+export default (props) => (
+  <div>
+    <h1>Page Number 0</h1>
+  </div>
+);
